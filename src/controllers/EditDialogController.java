3368989c69871a90f93429a9e5c9d6164d6a2505
erfaenda <regalko7@gmail.com>
2016-09-10 19:36:40 +0500

    Внес типо изменения

---
+++ src/controllers/EditDialogController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by alex on 10.09.2016.
+ */
+public class EditDialogController {
+
+    @FXML
+    private Label labelFio;
+
+    @FXML
+    private Label labelPhone;
+
+    @FXML
+    private TextField txtFio;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private Button btnOK;
+
+    @FXML
+    private Button btnCancel;
+
+}
--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -7,6 +7,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,7 +17,26 @@ import java.io.IOException;
 
 public class MainController {
     @FXML
-    private Button addButton;
+    private Button btnAdd;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnDel;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private TableView tableAdressBook;
+
+    @FXML
+    private Label labelCount;
+
     public void showDailog(ActionEvent actionEvent) {
 
         try {
