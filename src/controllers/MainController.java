5b7f58b6a341226d7a7cab45e3a10f62b09e512b
erfaenda <regalko7@gmail.com>
2016-09-10 19:13:55 +0500

    Внес типо изменения

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,16 +1,20 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController {
+    @FXML
+    private Button addButton;
     public void showDailog(ActionEvent actionEvent) {
 
         try {
